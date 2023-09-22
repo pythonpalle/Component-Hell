c217c4d1f9217346be33b8d057af35ec05f7bd42
pythonpalle <patrikwlindfors@gmail.com>
2023-09-22 16:06:33 +0200

    Add Force Upgrade Woho!

---
+++ Component-Hell-Project/Assets/AddForceToRigidbody.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceToRigidbody : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float force = 10;
+
+    public void AddForce()
+    {
+        if (_rigidbody2D)
+        {
+            Vector2 awayFromPlayer = ((Vector2)transform.position - PlayerMovementSingleton.Instance.Position).normalized;
+            _rigidbody2D.AddForce(awayFromPlayer * force, ForceMode2D.Impulse);
+        }
+    }
+}
